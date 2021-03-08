bdfe34a0df6f02ab6503f533622a6243c6453f2c
JontyBurger <cyber_learning@outlook.com>
2021-03-08 12:47:52 +0000

    Added the binary to the .gitignore file

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello and welcome to the GO home page!")
+}
 
 func main() {
-	fmt.Println("Hello and welcome to Go!")
+	// create the handler for the home page
+	http.HandleFunc("/", homeFunc)
+	http.ListenAndServe(":3000", nil)
 }
